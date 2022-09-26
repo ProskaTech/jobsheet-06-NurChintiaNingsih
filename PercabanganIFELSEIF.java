24d07780157a5d14cdee46b3b66b0b49a4c25499
NurChintiaNingsih <112674958+NurChintiaNingsih@users.noreply.github.com>
2022-09-26 17:24:14 +0700

    Add files via upload

---
+++ PercabanganIFELSEIF.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ * @author 21343011 - Nur Chintia Ningsih
+ */
+package JS6;
+
+public class PercabanganIFELSEIF {
+
+	public static void main(String[] args) {
+		double grade = 92.0;
+		
+		if(grade >= 90) {
+			System.out.println("Excellent!");
+			
+		}
+		/*Untuk menentukan kondisi yang mempunyai nilai rentangan maka
+		 tentukan nilai batas atas dan batas bawah setelah itu digabungkan
+		 dengan logika AND (&&)*/
+		 
+		else if ((grade < 90)&& (grade >= 80)) { // 80 s.d 89
+			System.out.println("Good Job!");
+		}
+		else if ((grade < 80)&& (grade >= 60)) { // 60 s.d 79
+			System.out.println("Study harder!");
+		}
+		else {
+			System.out.println("Sorry, you failed.");
+		}
+
+	}
+
+}
---
+++ PercabanganSwitchCase.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @author 21343011 - Nur Chintia Ningsih
+ */
+package JS6;
+
+public class PercabanganSwitchCase {
+
+	public static void main(String[] args) {
+		int grade = 92;
+			
+		switch (grade){
+			case 100:
+				System.out.println("Excellent!");
+				break;
+			case 90:
+				System.out.println("Good job!");
+				break;
+			case 80:
+				System.out.println("Study harder!");
+				break;
+			default:
+				System.out.println("Sorry, you failed.");
+		
+		}
+
+	}
+
+}
